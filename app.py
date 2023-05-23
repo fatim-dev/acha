0d083530569fa5e42699fa2ab2792ffb5b925f23
fatim-dev <129873648+fatim-dev@users.noreply.github.com>
2023-05-23 15:02:49 +0000

    submit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template 
-
+from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello_world():
   return render_template('home.html')
+
+@app.route("/", methods=['post'])
+def application_submitted():
+  data = request.form
+  return render_template('application_submited.html', application=data)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
